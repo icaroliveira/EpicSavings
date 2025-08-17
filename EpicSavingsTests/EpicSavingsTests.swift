c9ee4484979d05f230c06936a4346b03a2b29fa7
Ícaro Rangel <icarogin2@gmail.com>
2025-08-17 16:57:25 -0400

    Initial Commit

---
+++ EpicSavingsTests/EpicSavingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EpicSavingsTests.swift
+//  EpicSavingsTests
+//
+//  Created by Ícaro Rangel on 17/08/25.
+//
+
+import Testing
+@testable import EpicSavings
+
+struct EpicSavingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
